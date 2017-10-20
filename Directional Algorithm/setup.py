9c4283f975099cec5273c1c730396ee817d8f9c7
Carlos Castro <castro_carlos6@hotmail.com>
2017-10-20 19:42:09 -0400

    Directional algorithm (#1)
    * Created setup.py, fleshed out a few functions for path.py, still need to finish setPath using path object
    
    * deleted path module
    
    * created POI as object, added 'type' value, added path as array, created visited array, created POI array
    
    * Algorithm works in finding shortest path, still needs additional testing and comments
    
    * Finished comments in code, formatted code and function/variable names to fit PEP8 standard
    
    * created modules for Object Oriented flow, test cases file
    
    * Fixed infinite loop bug, changed convention from left, right, middle to left, middle, right
    
    * Fixed issue with test case 5, second shortest path was incorrect, it all works as it should, ready for integration with alarm comm

---
+++ Directional Algorithm/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Directional Algorithm',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/uknomecarlos/SMARTAlarmSW',
+    license='',
+    author='Carlos Castro and Lucas Plager',
+    author_email='castro_carlos6@hotmail.com',
+    description=''
+)
