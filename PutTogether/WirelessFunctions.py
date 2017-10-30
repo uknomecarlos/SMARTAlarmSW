1440c086588f23caa1ceee701c73571ae1f718bf
lucas <lplager@icloud.com>
2017-10-30 09:36:27 +0000

    minor changes to wireless

--- PutTogether/WirelessFunctions.py
+++ PutTogether/WirelessFunctions.py
@@ -1,4 +1,8 @@
 from xbee import ZigBee
+from twilio.rest import Client
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import time
 import serial
 from PointOfInterest import *
@@ -6,6 +10,20 @@ from path import *
 
 
 UNKNOWN = '\xff\xfe' # This is the 'I don't know' 16 bitA address
+
+# Setup sms text client with specified account details
+client = Client("AC591080d05afb9ed3ce18caa8217357b9","b373738564ac52c3cb981a14676f1d63")
+
+smsReceiver = "+17278719771"
+smsSender = "+13059129032"
+
+# Setup email settings for the smart alarm emergency messages
+fromaddr = 'smartalarmucf@yahoo.com'
+password = 'goknights'
+toaddr = 'lplager@icloud.com'
+smtp = 'smtp.mail.yahoo.com'
+port = 587
+
 
 def sendPacket(zb, where, what):
         # I'm only going to send the absolute minimum.
@@ -35,7 +53,26 @@ def signalReset(zb, all_pois):
                 time.sleep(0.005)    
                 if poi.get_type() != "exit" and poi.get_type() != "wall":
                         sendPacket(zb, poi.get_address(), 'reset')
-                        
+
+# Send an email packet
+def sendEmail(alarmID):
+        server = smtplib.SMTP(smtp, 587)
+        server.ehlo
+        server.starttls()
+        server.login(fromaddr, password)
+
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = "Emergency SmartAlarm Alert!"
+        body = ('The SmartAlarm system has detected a fire emergency at alarm ' + alarmID + '. Alarms have been triggered!')
+
+        msg.attach(MIMEText(body, 'plain'))
+
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit()
+
 # OK, another thread has caught the packet from the XBee network,
 # put it on a queue, this process has taken it off the queue and
 # passed it to this routine, now we can take it apart and see
@@ -55,5 +92,7 @@ def handlePacket(data, all_pois, visited, zb):
                         fire_alarm(poi, visited)
                         print_each_direction(all_pois)
                         signalDirections(zb, all_pois)
+                        client.messages.create(to=smsReceiver, from_=smsSender, body="There is a fire at alarm " + str(poi.get_id()) + "!!!")
+                        sendEmail(str(poi.get_id()))
         else:
                 print 'Unimplemented frame type'
